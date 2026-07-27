4ab0545603a6b20dd8bdf6a62e12f970c1b348d4
Thelawmens <>
2026-07-27 16:20:51 +0530

    Fix_SlowBuffer_Node20_and_start_script

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if (typeof global.SlowBuffer === 'undefined') {
+    const { Buffer } = require('buffer');
+    global.SlowBuffer = Buffer;
+}
+
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
