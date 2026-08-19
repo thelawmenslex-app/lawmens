932365b42880bd80dcb21aa577f658317add4160
Thelawmens <thelawmenslex@gmail.com>
2026-08-19 15:52:03 +0530

    Allow public GET for category list - removed auth requirement

--- src/category/category.route.js
+++ src/category/category.route.js
@@ -3,8 +3,10 @@ const { validate } = require('../../middleware/validation');
 const categoryController = require('./category.controller');
 const { addCategory } = require('./category.validation');
 const router = express.Router();
-const { auth } = require("../../middleware/auth.middleware")
+const { auth } = require("../../middleware/auth.middleware");
+
 router.post('/', auth, validate(addCategory), categoryController.addEndUse);
-router.get('/', auth, categoryController.getEndUses);
-router.get('/:categoryId', auth, categoryController.getEndUseById);
+router.get('/', categoryController.getEndUses);
+router.get('/:categoryId', categoryController.getEndUseById);
+
 module.exports = router;
