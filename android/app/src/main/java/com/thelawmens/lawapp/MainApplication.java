468504521092234fcee2dfa8286cfa0755a4cd8e
Thelawmens <thelawmenslex@gmail.com>
2026-08-20 23:00:52 +0530

    Fix: Configure standalone embedded bundle to eliminate Metro dev server red screen dependency forever

--- android/app/src/main/java/com/thelawmens/lawapp/MainApplication.java
+++ android/app/src/main/java/com/thelawmens/lawapp/MainApplication.java
@@ -16,7 +16,7 @@ public class MainApplication extends Application implements ReactApplication {
       new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
+          return false;
         }
 
         @Override
